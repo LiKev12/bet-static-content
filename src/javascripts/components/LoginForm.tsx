a8e8e40795b081e290291f720a1ce021f7a4c1da
Kevin Li <kevinli1258@gmail.com>
2024-03-22 13:08:23 -0700

    first commit to prod

--- src/javascripts/components/LoginForm.tsx
+++ src/javascripts/components/LoginForm.tsx
@@ -33,7 +33,7 @@ const LoginForm: React.FC = () => {
     const sliceAuthenticationState = useSelector((state: IRootState) => state.authentication);
     const sliceAuthenticationStateResponse = new ResponseModel(sliceAuthenticationState.response);
     const [loginFormState, setLoginFormState] = useState<ILoginFormState>({
-        data: { username: '', password: '' },
+        data: { username: 'production', password: '' },
         isShowVisiblePassword: false,
         isShowOptionsForgotPassword: false,
         isShowOptionsForgotUsername: false,
